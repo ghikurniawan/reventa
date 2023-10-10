cbc4b797fd563718df69e74c77f91df66ee2cf5d
ikurniawan <business.ikurniawan@gmail.com>
2023-10-10 20:49:14 +0700

    make it responsive

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -39,14 +39,24 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   }, [index, images]);
 
   return (
-    <div className="w-full relative bg-[url('/paralax-bg.jpg')] h-fit bg-no-repeat bg-cover">
-        <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
-        <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
-        <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
-        <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 left-1/4' width={1200} />
-        <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 left-1/4' width={1200} />
-        <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 left-1/4' width={1200} />
-        <div className='mt-80 absolute left-0 top-0 right-0 bottom-0 '>
+    <div className="w-full relative bg-[url('/paralax-bg.jpg')] h-fit bg-no-repeat bg-cover max-h-screen">
+        <div className=" hidden md:block">
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 right-0' width={1000} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 right-0' width={1000} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 right-0' width={1000} />
+        </div>
+        <div className="md:hidden">
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
+        </div>
+        <div className='mt-52 lg:mt-80 absolute left-0 top-0 right-0 bottom-0 '>
           <AnimatePresence mode='wait'>
             <motion.div
               key={selectedTab ? selectedTab.id : "empty"}
@@ -66,12 +76,12 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
             </motion.div>
           </AnimatePresence>
         </div>
-        <div className="mt-60 z-10 min-h-screen max-w-screen-xl mx-auto relative flex justify-between">
-          <div className='max-w-lg space-y-8'>
-            <h1 className={`text-5xl ${tenor.className}`}>Our Home Defines Us</h1>
-            <p className='text-xl'>Revanta Homes is a project by Rishabhdeo Builders & Developers. They build homes with timeless designs. Thet build homes which are beyond your dreams.</p>
+        <div className="mt-32 lg:mt-60 z-10 lg:min-h-screen max-w-screen-xl mx-auto relative flex flex-col lg:flex-row justify-between">
+          <div className='max-w-lg space-y-4 lg:space-y-8 p-2'>
+            <h1 className={`text-3xl lg:text-5xl text-center lg:text-left ${tenor.className}`}>Our Home Defines Us</h1>
+            <p className='text-md lg:text-xl'>Revanta Homes is a project by Rishabhdeo Builders & Developers. They build homes with timeless designs. Thet build homes which are beyond your dreams.</p>
           </div>
-            <nav className='w-2/4' >
+            <nav className='w-2/4 min-w-[380px] mt-32 md:mt-0 p-2' >
               <motion.ul drag='x' dragConstraints={{left: 0 , right: 0}} className='w-full grid grid-cols-3 gap-2'>
                 {images.map((item, index) => (
                     <li
@@ -79,7 +89,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
                       className={`${item === selectedTab ? "relative" : ""} w-full h-full`}
                       onClick={() => (setSelectedTab(item), setIndex(index))}
                     >
-                      <AspectRatio ratio={4/3} className='relative col-span-2'>
+                      <AspectRatio ratio={4/3} className='relative col-span-2 bg-white shadow-lg'>
                         <Image 
                         src={item?.thumbnail} 
                         draggable={false}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,27 +11,31 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 import useScroll from "@/lib/hooks/use-scroll";
+import useWindowSize from "@/lib/hooks/use-window-size";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
   const scrolled = useScroll(20);
+  const {isMobile} = useWindowSize()
+
   return (
     <div className={`fixed top-0 w-full z-50 ${scrolled ? "bg-white bg-opacity-30 backdrop-blur-lg" : ""}`}>
-      <nav className="flex justify-between items-center my w-full max-w-screen-xl mx-auto h-[110px]">
-        <div className="flex gap-28 items-center">
-          <Image src="/logo.png" alt="Reventa Logo" height={100} width={100} />
-          <ul className="flex gap-10 text-brand">
+      {isMobile ? (
+        <div className="w-full flex justify-center pt-2">
+            <Image src="/logo.png" alt="Reventa Logo" height={50} width={50} />
+        </div>
+      ) : ""}
+      <nav className="flex justify-between items-center my w-full max-w-screen-xl mx-auto h-20 lg:h-[110px] p-2">
+        <div className="flex gap-2 lg:gap-28 items-center">
+          <Image src="/logo.png" alt="Reventa Logo" height={100} width={100} className="hidden lg:block" />
+          <ul className="flex gap-2 lg:gap-10 text-brand">
             <li>
               <Link href={"/"}>Home</Link>
             </li>
@@ -41,15 +45,19 @@ const Navbar: FC<NavbarProps> = () => {
           </ul>
         </div>
         <div className="flex gap-4">
-          <Button variant={"brand"}>
+          <Button variant={"brand"} size={'sm'}>
             <ArrowDownIcon className="mr-4" /> Download Brochure
           </Button>
 
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="border-none"> <GlobeIcon className="mr-4" /> English </Button>
+              {isMobile ? (
+                <Button variant="outline" size={'icon'} className="border-none"> <GlobeIcon className="mr-4" /> </Button>
+              ):(
+                <Button variant="outline" className="border-none"> <GlobeIcon className="mr-4" /> English</Button>
+              )}
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
+            <DropdownMenuContent className="lg:w-56">
               <DropdownMenuLabel>Language</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuGroup>
