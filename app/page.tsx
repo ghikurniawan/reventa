7446717e403f31a3f8b5ad2c3306ab18b6c63a29
ikurniawan <business.ikurniawan@gmail.com>
2023-10-11 17:07:40 +0700

    add features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import Carousel from '@/components/Carousel';
+import Features from '@/components/Features';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   const images = [
     { id: 1, thumbnail: '/BG.png', full: '/BG.png', title: 'TULIP' },
-    { id: 2, thumbnail: '/BG2.png', full: '/BG2.png', title: 'ROSE' },
-    { id: 3, thumbnail: '/BG3.png', full: '/BG3.png', title: 'LILY' },
+    { id: 2, thumbnail: '/BG2.png', full: '/BG2.png', title: 'ORCHID' },
+    { id: 3, thumbnail: '/BG3.png', full: '/BG3.png', title: 'IRIS' },
+    // ...
+  ];
+  const featuresImage = [
+    { id: 1, thumbnail: '/features/features_1.png', full: '/features/features_1.png', title: 'Park1' },
+    { id: 2, thumbnail: '/features/features_2.png', full: '/features/features_2.png', title: 'Park2' },
+    { id: 3, thumbnail: '/features/features_3.png', full: '/features/features_3.png', title: 'Park3' },
+    { id: 3, thumbnail: '/features/features_4.png', full: '/features/features_4.png', title: 'Park4' },
     // ...
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Carousel images={images} />
+      <Hero images={images} />
+      <Features featuresImage={featuresImage} />
     </main>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,10 +2,11 @@
 
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import Image from 'next/image'
-import React, { FC, useEffect, useRef, useState } from 'react'
-import { AnimatePresence, motion, useTransform, MotionValue, useScroll } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+import { AnimatePresence, motion, useTransform, useScroll } from 'framer-motion'
 import { Tenor_Sans } from 'next/font/google'
 const tenor = Tenor_Sans({ subsets: ['latin'], weight: "400" })
+
 type Image = {
   id: number;
   thumbnail: string;
@@ -13,19 +14,15 @@ type Image = {
   title: string;
 };
 
-type CarouselProps = {
+type HeroProps = {
   images: Image[];
 };
 
-const Carousel: React.FC<CarouselProps> = ({ images }) => {
+const Hero: React.FC<HeroProps> = ({ images }) => {
   const [selectedTab, setSelectedTab] = useState(images[0]);
   const [index, setIndex] = useState(0)
   let { scrollYProgress } = useScroll();
   let y = useTransform(scrollYProgress, [0,1], ['0%', '80%'])
-
-  useEffect(()=>{
-    console.log(scrollYProgress)
-  }, [scrollYProgress])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -39,7 +36,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   }, [index, images]);
 
   return (
-    <div className="w-full relative bg-[url('/paralax-bg.jpg')] h-fit bg-no-repeat bg-cover max-h-screen">
+    <div className="w-full relative bg-[url('/paralax-bg.jpg')] bg-no-repeat bg-cover min-h-screen">
         <div className=" hidden md:block">
           <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
           <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-20 left-0' width={600} />
@@ -49,14 +46,14 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
           <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-40 right-0' width={1000} />
         </div>
         <div className="md:hidden">
-          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
-          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
-          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-10 left-0' width={200} />
-          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
-          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
-          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-10 right-20' width={400} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-52 left-0' width={200} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-52 left-0' width={200} />
+          <motion.img style={{y}} src='cloud.png' alt='cloud' className='absolute top-52 left-0' width={200} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-1/2 right-2' width={400} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-1/2 right-2' width={400} />
+          <motion.img style={{y}} src='cloud2.png' alt='cloud' className='absolute top-1/2 right-2' width={400} />
         </div>
-        <div className='mt-52 lg:mt-80 absolute left-0 top-0 right-0 bottom-0 '>
+        <div className='mt-52 lg:mt-60 absolute left-0 lg:top-0 right-0 bottom-32 md:bottom-0 max-h-screen'>
           <AnimatePresence mode='wait'>
             <motion.div
               key={selectedTab ? selectedTab.id : "empty"}
@@ -76,12 +73,12 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
             </motion.div>
           </AnimatePresence>
         </div>
-        <div className="mt-32 lg:mt-60 z-10 lg:min-h-screen max-w-screen-xl mx-auto relative flex flex-col lg:flex-row justify-between">
-          <div className='max-w-lg space-y-4 lg:space-y-8 p-2'>
+        <div className="mt-32 h-full min-h-[80vh] lg:mt-60 z-10 lg:min-h-screen max-w-screen-xl mx-auto relative flex flex-col lg:flex-row justify-between">
+          <div className='space-y-4 lg:space-y-8 p-2'>
             <h1 className={`text-3xl lg:text-5xl text-center lg:text-left ${tenor.className}`}>Our Home Defines Us</h1>
-            <p className='text-md lg:text-xl'>Revanta Homes is a project by Rishabhdeo Builders & Developers. They build homes with timeless designs. Thet build homes which are beyond your dreams.</p>
+            <p className='text-md lg:text-xl max-w-xl'>Revanta Homes is a project by Rishabhdeo Builders & Developers. They build homes with timeless designs. Thet build homes which are beyond your dreams.</p>
           </div>
-            <nav className='w-2/4 min-w-[380px] mt-32 md:mt-0 p-2' >
+            <nav className='w-2/4 min-w-[380px] max-w-md md:mt-0 p-2' >
               <motion.ul drag='x' dragConstraints={{left: 0 , right: 0}} className='w-full grid grid-cols-3 gap-2'>
                 {images.map((item, index) => (
                     <li
@@ -115,4 +112,4 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   )
 }
 
-export default Carousel;
+export default Hero;
